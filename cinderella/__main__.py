f62cbb01332c59b66ce2d28a211e7580d9926e10
Jerin Johny <71542613+jerinjohny-ktnm@users.noreply.github.com>
2020-10-16 09:56:15 +0530

    Update for __main__.py

--- cinderella/__main__.py
+++ cinderella/__main__.py
@@ -29,7 +29,7 @@ from cinderella.modules.connection import connect_button
 PM_START_TEXT = """
 _Hello_ *{}*
 _My name is_ *{}*\n_A Powerful Telegram ProBot to Manage Your Groups,feel free to add to your groups!!_
-_Maintained by_ [{}](tg://user?id={})
+_Maintained by_ *{}*
 """
 
 
